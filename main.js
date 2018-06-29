3636fa0c982b13b7c9695e0cbf1f5515776747d0
Stef Egbert <stephenegbert@gmail.com>
2018-06-29 18:05:57 -0400

    fixed it

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('Javascript has loaded');
+
+function enableSepia() {
+  alert("Sepia theme has been Enabled.");
+  var x = document.getElementById("page");
+  x.classList.add("sepia");
+}
